7cb239172d9c671fcf55d5d93892953f837b3883
Tyler Bettilyon <teb311@gmail.com>
2015-11-10 18:46:54 -0800

    working, but hidious

--- javascript/app.js
+++ javascript/app.js
@@ -60,11 +60,87 @@ function bootstrapSpotifySearch(){
 function displayAlbumsAndTracks(event) {
   var appendToMe = $('#albums-and-tracks');
 
-  // These two lines can be deleted. They're mostly for show. 
-  console.log("you clicked on:");
-  console.log($(event.target).attr('data-spotify-id'));//.attr('data-spotify-id'));
+  var simplifiedData = {};
+
+  // Get all the albums
+  getAllAlbums($(event.target).attr('data-spotify-id'))
+  
+  // Then fetch the data for the individual albums
+  .then(function(allAlbumData) {
+    console.log(allAlbumData);
+    var albumPromises = [];
+
+    for(var i = 0; i < allAlbumData.items.length; i++) {
+      var albumId = allAlbumData.items[i].id;
+      simplifiedData[allAlbumData.items[i].name] = {};
+
+      var albumPromise = getAlbumInfo(albumId);
+      albumPromise.then(function(albumData) {
+        console.log(albumData);
+
+        simplifiedData[albumData.name]['releaseDate'] = albumData.release_date;
+        simplifiedData[albumData.name]['tracks'] = []
+
+        var trackPromise = getAllTracks(albumId);
+
+        trackPromise.then(function(tracksData) {
+          console.log(tracksData);
+          trackNames = tracksData.items.map(function(track) {
+            return track.name;
+          });
+          simplifiedData[albumData.name]['tracks'] = trackNames;
+          return trackNames;
+        });
+
+        return trackPromise;
+      });
+
+      albumPromises.push(albumPromise);
+    }
+
+    console.log(albumPromises);
+    return $.when.apply($, albumPromises);
+  })
+  // when all the albums have data
+  // We'll already have the tracks
+  .done(function() {
+    console.log(simplifiedData);
+  });
 }
 
 /* YOU MAY WANT TO CREATE HELPER FUNCTIONS OF YOUR OWN */
 /* THEN CALL THEM OR REFERENCE THEM FROM displayAlbumsAndTracks */
 /* THATS PERFECTLY FINE, CREATE AS MANY AS YOU'D LIKE */
+function getAllAlbums(artistId) {
+  var searchUrl = 'https://api.spotify.com/v1/artists/' + artistId + '/albums';
+  var allAlbumsPromise = $.ajax({
+          type: "GET",
+          dataType: 'json',
+          url: searchUrl
+    });
+
+  return allAlbumsPromise;
+}
+
+function getAlbumInfo(albumId) {
+  var searchUrl = 'https://api.spotify.com/v1/albums/' + albumId;
+  var albumPromise = $.ajax({
+          type: "GET",
+          dataType: 'json',
+          url: searchUrl
+    });
+
+  return albumPromise;
+}
+
+function getAllTracks(albumId) {
+  var searchUrl = 'https://api.spotify.com/v1/albums/' + albumId + '/tracks';
+  var allTrackssPromise = $.ajax({
+          type: "GET",
+          dataType: 'json',
+          url: searchUrl
+    });
+
+  return allTrackssPromise;
+}
+
