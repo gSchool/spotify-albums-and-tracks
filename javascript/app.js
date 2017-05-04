dfe7cb2f164fecffd284c45058e18228e5a00da0
Alex Read <alexread730@gmail.com>
2017-05-04 16:26:24 -0600

    added drop down albums

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 // Self envoking function! once the document is ready, bootstrap our application.
-// We do this to make sure that all the HTML is rendered before we do things 
-// like attach event listeners and any dom manipulation.  
+// We do this to make sure that all the HTML is rendered before we do things
+// like attach event listeners and any dom manipulation.
 (function(){
   $(document).ready(function(){
     bootstrapSpotifySearch();
@@ -27,7 +27,7 @@ function bootstrapSpotifySearch(){
           url: searchUrl
       });
 
-      // Attach the callback for success 
+      // Attach the callback for success
       // (We could have used the success callback directly)
       spotifyQueryRequest.done(function (data) {
         var artists = data.artists;
@@ -35,7 +35,7 @@ function bootstrapSpotifySearch(){
         // Clear the output area
         outputArea.html('');
 
-        // The spotify API sends back an arrat 'items' 
+        // The spotify API sends back an arrat 'items'
         // Which contains the first 20 matching elements.
         // In our case they are artists.
         artists.items.forEach(function(artist){
@@ -47,7 +47,7 @@ function bootstrapSpotifySearch(){
         })
       });
 
-      // Attach the callback for failure 
+      // Attach the callback for failure
       // (Again, we could have used the error callback direcetly)
       spotifyQueryRequest.fail(function (error) {
         console.log("Something Failed During Spotify Q Request:")
@@ -60,9 +60,45 @@ function bootstrapSpotifySearch(){
 function displayAlbumsAndTracks(event) {
   var appendToMe = $('#albums-and-tracks');
 
-  // These two lines can be deleted. They're mostly for show. 
+  // These two lines can be deleted. They're mostly for show.
   console.log("you clicked on:");
   console.log($(event.target).attr('data-spotify-id'));//.attr('data-spotify-id'));
+
+  //artist clicked
+  var clicked = $(event.target);
+
+
+  $.getJSON('https://api.spotify.com/v1/artists/' + $(event.target).attr('data-spotify-id') + '/albums').then(function(response) {
+
+    var albums = response.items;
+    console.log(response);
+    console.log(albums);
+    var ul = document.createElement('ul');
+    clicked.append(ul);
+
+    for (var i = 0; i < albums.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.innerText = albums[i].name;
+      listItem.style.textDecoration = 'none';
+      ul.append(listItem);
+    }
+
+    // //track request
+    // albums.forEach(function(album) {
+    //   $.getJSON('https://api.spotify.com/v1/albums/' + album.id + '/tracks').then(function(response) {
+    //
+    //     var tracks = response.items;
+    //     //console.log(tracks);
+    //     for (var i = 0; i < tracks.length; i++) {
+    //       console.log(tracks[i].name);
+    //     }
+    //
+    //
+    //
+    //   });
+    // });
+
+  });
 }
 
 /* YOU MAY WANT TO CREATE HELPER FUNCTIONS OF YOUR OWN */
